d954f7e5363f2e4d6c157cde91caf1c5b7b7f4d0
Josh Fyne <josh.fyne@humanmademachine.com>
2020-10-25 11:53:14 +0000

    Implementing focus, blur, window-focus, window-blur

--- web/src/events.ts
+++ web/src/events.ts
@@ -1,31 +1,115 @@
 import { Socket } from "./socket";
-import { LiveElement } from "./element";
+import { LiveValues, LiveElement } from "./element";
 
-/**
- * handles a "live-click" click event.
- */
-function clickHandler(this: HTMLElement, _: Event) {
-    //const element = e.target as HTMLElement;
-    const t = this?.getAttribute("live-click");
-    if (t === null) {
-        return;
+class EventHandler {
+    constructor(protected event: string, protected attribute: string) {}
+
+    public isWired(element: Element): boolean {
+        if (element.hasAttribute(`${this.attribute}-wired`)) {
+            return true;
+        }
+        element.setAttribute(`${this.attribute}-wired`, "");
+        return false;
     }
-    const values = LiveElement.values(this);
-    Socket.send({ t: t, d: values });
+
+    public attach() {
+        document
+            .querySelectorAll(`*[${this.attribute}]`)
+            .forEach((element: Element) => {
+                if (this.isWired(element) == true) {
+                    return;
+                }
+                const values = LiveElement.values(element as HTMLElement);
+                element.addEventListener(
+                    this.event,
+                    this.handler(element as HTMLElement, values)
+                );
+            });
+    }
+
+    protected handler(element: HTMLElement, values: LiveValues) {
+        return (_: Event) => {
+            const t = element?.getAttribute(this.attribute);
+            if (t === null) {
+                return;
+            }
+            Socket.send({ t: t, d: values });
+        };
+    }
 }
 
 /**
  * live-click attribute handling.
  */
-export class Click {
-    /**
-     * Attaches handlers to all live-click attributes in
-     * the DOM
-     */
-    public static attach() {
-        document.querySelectorAll("*[live-click]").forEach((element) => {
-            element.addEventListener("click", clickHandler);
-        });
+export class Click extends EventHandler {
+    constructor() {
+        super("click", "live-click");
+    }
+}
+
+/**
+ * live-focus event handling.
+ */
+export class Focus extends EventHandler {
+    constructor() {
+        super("focus", "live-focus");
+    }
+}
+
+/**
+ * live-blur event handling.
+ */
+export class Blur extends EventHandler {
+    constructor() {
+        super("blur", "live-blur");
+    }
+}
+
+/**
+ * live-window-focus event handler.
+ */
+export class WindowFocus extends EventHandler {
+    constructor() {
+        super("focus", "live-window-focus");
+    }
+
+    public attach() {
+        document
+            .querySelectorAll(`*[${this.attribute}]`)
+            .forEach((element: Element) => {
+                if (this.isWired(element) === true) {
+                    return;
+                }
+                const values = LiveElement.values(element as HTMLElement);
+                window.addEventListener(
+                    this.event,
+                    this.handler(element as HTMLElement, values)
+                );
+            });
+    }
+}
+
+/**
+ * live-window-blur event handler.
+ */
+export class WindowBlur extends EventHandler {
+    constructor() {
+        super("blur", "live-window-blur");
+    }
+
+    public attach() {
+        document
+            .querySelectorAll(`*[${this.attribute}]`)
+            .forEach((element: Element) => {
+                if (this.isWired(element) === true) {
+                    return;
+                }
+                const values = LiveElement.values(element as HTMLElement);
+                window.addEventListener(
+                    this.event,
+                    this.handler(element as HTMLElement, values)
+                );
+            });
     }
 }
 
@@ -33,10 +117,31 @@ export class Click {
  * Handle all events.
  */
 export class Events {
+    private static clicks: Click;
+    private static focus: Focus;
+    private static blur: Blur;
+    private static windowFocus: WindowFocus;
+    private static windowBlur: WindowBlur;
+
+    /**
+     * Initialise all the event wiring.
+     */
+    public static init() {
+        this.clicks = new Click();
+        this.focus = new Focus();
+        this.blur = new Blur();
+        this.windowFocus = new WindowFocus();
+        this.windowBlur = new WindowBlur();
+    }
+
     /**
      * Re-attach all events when we have re-rendered.
      */
     public static rewire() {
-        Click.attach();
+        this.clicks.attach();
+        this.focus.attach();
+        this.blur.attach();
+        this.windowFocus.attach();
+        this.windowBlur.attach();
     }
 }

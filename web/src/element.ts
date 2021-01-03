d23b952b2d3af010fbb423eae5ec1ed5b48ed371
Josh Fyne <josh.fyne@humanmademachine.com>
2021-01-03 15:41:34 +0000

    fix: Firefox issue on forms

--- web/src/element.ts
+++ web/src/element.ts
@@ -28,6 +28,9 @@ export class LiveElement {
     }
 
     static hook(element: HTMLElement): string | null {
+        if (element.getAttribute === undefined) {
+            return null;
+        }
         return element.getAttribute("live-hook");
     }
 }

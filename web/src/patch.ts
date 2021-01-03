90e6ab2171f270f507da2e3d0fdd55a315067a39
Josh Fyne <josh.fyne@humanmademachine.com>
2021-01-03 17:03:17 +0000

    fix: Firefox leaves empty text nodes about. This breaks our patch paths.

--- web/src/patch.ts
+++ web/src/patch.ts
@@ -51,21 +51,27 @@ export class Patch {
                 if (target.parentNode === null) {
                     return;
                 }
+                Patch.mergeEmptyNodes(target.parentNode.childNodes);
                 EventDispatch.beforeUpdate(target);
                 target.parentNode.insertBefore(Patch.html2Node(e.HTML), target);
                 EventDispatch.updated(target);
                 break;
             case 2: // REPLACE
+                if (target.parentNode) {
+                    Patch.mergeEmptyNodes(target.parentNode.childNodes);
+                }
                 EventDispatch.beforeDestroy(target);
                 target.outerHTML = e.HTML;
                 EventDispatch.destroyed(target);
                 break;
             case 3: // APPEND
+                Patch.mergeEmptyNodes(target.childNodes);
                 EventDispatch.beforeUpdate(target);
                 target.append(Patch.html2Node(e.HTML));
                 EventDispatch.updated(target);
                 break;
             case 4: // PREPEND
+                Patch.mergeEmptyNodes(target.childNodes);
                 EventDispatch.beforeUpdate(target);
                 target.prepend(Patch.html2Node(e.HTML));
                 EventDispatch.updated(target);
@@ -81,5 +87,35 @@ export class Patch {
             return document.createTextNode(html);
         }
         return template.content.firstChild;
+    }
+
+    /**
+     * Firefox appears to leave dangling empty nodes next to each other
+     * unlike Chrome. This should check for such nodes and remove them
+     * so that our paths continue to work.
+     */
+    private static mergeEmptyNodes(nodeList: NodeList) {
+        let done = false;
+        while (done === false) {
+            done = true;
+            let p: Text = nodeList[0] as Text;
+            for (let i = 0; i < nodeList.length; i++) {
+                if (i == 0) {
+                    p = nodeList[i] as Text;
+                    continue;
+                }
+                if (p.nodeType === 3 && p.data.trim() === "") {
+                    if (
+                        nodeList[i].nodeType === 3 &&
+                        (nodeList[i] as Text).data.trim() === ""
+                    ) {
+                        (nodeList[i] as ChildNode).remove();
+                        done = false;
+                        break;
+                    }
+                }
+                p = nodeList[i] as Text;
+            }
+        }
     }
 }

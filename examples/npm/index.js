261aed7d1c76ce7514729970b16f474394f7c211
Josh Fyne <josh.fyne@humanmademachine.com>
2021-01-15 18:05:18 +0000

    Clearer example of importing live and using a hook

--- examples/npm/index.js
+++ examples/npm/index.js
@@ -1,5 +1,13 @@
 import { Live } from "@jfyne/live";
 
 document.addEventListener("DOMContentLoaded", (_) => {
-    new Live();
+    const hooks = {
+        "example-hook": {
+            mounted: () => {
+                console.log("This is an example of passing hooks into live anywhere you want.");
+            }
+        }
+    };
+    const l = new Live(hooks);
+    l.init();
 });

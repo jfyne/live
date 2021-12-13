5f030fd88a0897a27ce41f744af5c6f8b7e93af7
Josh Fyne <josh.fyne@gmail.com>
2021-12-13 07:52:40 +0000

    feat: Build script now installs node_modules if missing.
    Refs:
     - https://github.com/jfyne/live/issues/40

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+if ! [ -d web/node_modules ]; then
+    cd web && npm install
+    cd -
+fi
 go generate web/build.go
 go generate internal/embed/embed.go
 if ! command -v embedmd &> /dev/null
